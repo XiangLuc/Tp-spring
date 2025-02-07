08c867f82bc65f4ffec0f1bffa6759baa5b91f9f
XiangLuc <xiangluc01@gmail.com>
2025-02-07 10:46:36 +0100

    Mise en place du autowired byName

--- src/main/java/tp1/DemandeCongeSansModificateurs.java
+++ src/main/java/tp1/DemandeCongeSansModificateurs.java
@@ -4,27 +4,22 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 
 public class DemandeCongeSansModificateurs {
 
-    private Collaborateur collaborateur;
-    private Periode periode;
-
-    public DemandeCongeSansModificateurs(Collaborateur collaborateur, Periode periode) {
-        this.collaborateur = collaborateur;
-        this.periode = periode;
-    }
+    private Collaborateur c;
+    private Periode p;
 
     public Collaborateur getCollaborateur() {
-        return collaborateur;
+        return c;
     }
 
     public Periode getPeriode() {
-        return periode;
+        return p;
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .append("Collaborateur", collaborateur)
-                .append("Période", periode)
+                .append("Collaborateur", c)
+                .append("Période", p)
                 .toString();
     }
 }
--- src/main/java/tp2/Main.java
+++ src/main/java/tp2/Main.java
@@ -1,7 +1,37 @@
 package tp2;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import tp1.Collaborateur;
+import tp1.DemandeConge;
+import tp1.Periode;
+
 public class Main {
     public static void main(String[] args) {
 
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Collaborateur collaborateur = (Collaborateur) applicationContext.getBean("c");
+        Periode periode = (Periode) applicationContext.getBean("p");
+        DemandeConge demandeConge = (DemandeConge) applicationContext.getBean("demandeConge");
+
+        System.out.println(collaborateur);
+        System.out.println(periode);
+        System.out.println(demandeConge);
+
     }
 }
+
+/*
+    Questions
+
+    Que se passe t'il lorsque l'injection des collaborateurs dans « DemandeConge »
+    est faite « byName » ?
+
+    Que faudrait-il changer pour pouvoir utiliser ce type d'injection?
+
+    Pour utiliser ce type d'injection il faut que les variables de la classe DemandeongeSansModificateurs.java aient les meme noms
+    que les id des beans de Periode et Collaborateur soit p et c.
+
+
+*/
