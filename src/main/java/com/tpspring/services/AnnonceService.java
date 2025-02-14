810d2abe22a904fd3573fd4b48828083cccb07fb
XiangLuc <xiangluc01@gmail.com>
2025-02-14 13:22:09 +0100

    Ajout des annotations de transaction

--- src/main/java/com/tpspring/services/AnnonceService.java
+++ src/main/java/com/tpspring/services/AnnonceService.java
@@ -4,6 +4,7 @@ import com.tpspring.entities.Annonce;
 import com.tpspring.repositories.AnnonceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -25,10 +26,12 @@ public class AnnonceService {
         return annonceRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public Annonce saveAnnonce(Annonce annonce) {
         return annonceRepository.save(annonce);
     }
 
+    @Transactional
     public void deleteAnnonce(Long id) {
         annonceRepository.deleteById(id);
     }
