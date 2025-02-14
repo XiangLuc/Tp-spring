2d9026a4175a57a04ec466aa244c384309d94daf
XiangLuc <xiangluc01@gmail.com>
2025-02-14 15:20:37 +0100

    Utilisation des logs dans le main.

--- src/main/java/com/tpspring/Main.java
+++ src/main/java/com/tpspring/Main.java
@@ -2,12 +2,13 @@ package com.tpspring;
 
 import com.tpspring.configuration.AppConfig;
 import com.tpspring.services.AnnonceService;
+import com.tpspring.utils.LoggerAspect;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class, LoggerAspect.class);
 
         AnnonceService annonceService = context.getBean(AnnonceService.class);
 
