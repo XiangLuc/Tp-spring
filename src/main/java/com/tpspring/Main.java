d02f902de2d9b805724a8a8061b85a997dcc338e
XiangLuc <xiangluc01@gmail.com>
2025-02-14 11:07:44 +0100

    Main de test pour voir si les annotations injecte correctement, et si la base de donnée est bien connecté.

---
+++ src/main/java/com/tpspring/Main.java
@@ -0,0 +1,20 @@
+package com.tpspring;
+
+import com.tpspring.config.JpaConfig;
+import com.tpspring.services.AnnonceService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JpaConfig.class);
+
+        AnnonceService annonceService = context.getBean(AnnonceService.class);
+
+        annonceService.getAllAnnonces().forEach(System.out::println);
+
+        context.close();
+
+    }
+}
+
